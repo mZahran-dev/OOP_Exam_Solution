ab1023172705c82e80536dbd9478a3b2f1563754
DELL <DELL@DESKTOP-M4KTLBQ>
2024-07-31 17:48:14 +0300

    feat: Adding CreateTrueFalseQuestion in QuestionFactory Class

--- OOP_Exam/QuestionFactory/QuestionFactory.cs
+++ OOP_Exam/QuestionFactory/QuestionFactory.cs
@@ -8,6 +8,47 @@ namespace OOP_Exam.QuestionFactory
 {
     internal class QuestionFactory
     {
+        #region Methods
+        public static TrueOrFalseQuestion CreateTrueFalseQuestion()
+        {
+            Console.WriteLine("True | False Question");
+            Console.Write("Enter Question Body: ");
+            string? body = Console.ReadLine();
+            Console.Write("Enter Question Mark: ");
+            double mark = double.Parse(Console.ReadLine());
+
+            var answers = new List<Answer>
+            {
+                new Answer(1, "True"),
+                new Answer(2, "False")
+            };
+
+            Console.Write("Enter The right Answer ID (1 for True, 2 for False): ");
+            int? correctAnswerId = int.Parse(Console.ReadLine());
+
+            Answer? correctAnswer = null;
+            foreach (var answer in answers)
+            {
+                if (answer.AnswerId == correctAnswerId)
+                {
+                    correctAnswer = answer;
+                    break;
+                }
+            }
+            if (correctAnswer == null)
+            {
+                throw new ArgumentException("Correct answer not found in provided answers.");
+            }
+
+
+
+            return new TrueOrFalseQuestion(body, mark, answers, correctAnswer);
+
+        }
+
         
+        #endregion
+
+
     }
 }
