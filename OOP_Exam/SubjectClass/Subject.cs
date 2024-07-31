79433fbf6d944f5019faf7263f04e5611ce98e4a
DELL <DELL@DESKTOP-M4KTLBQ>
2024-07-31 16:24:12 +0300

    feat: initialize Subject Class with basic Propeties

--- OOP_Exam/SubjectClass/Subject.cs
+++ OOP_Exam/SubjectClass/Subject.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OOP_Exam.Exam_Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,30 @@ namespace OOP_Exam.SubjectClass
 {
     internal class Subject
     {
+        #region Propeties
+        private int subjectId;
+        private string? subjectName;
+        private Exam? subjectExam;
+
+        public Exam? SubjectExam
+        {
+            get { return subjectExam; }
+            set { subjectExam = value; }
+        }
+
+        public string? SubjectName
+        {
+            get { return subjectName; }
+            set { subjectName = value; }
+        }
+
+        public int SubjectId
+        {
+            get { return subjectId; }
+            set { subjectId = value; }
+        }
+        #endregion Propeties
+
+
     }
 }
