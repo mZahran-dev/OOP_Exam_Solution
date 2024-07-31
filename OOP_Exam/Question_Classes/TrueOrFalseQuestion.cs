d44ac95ce7c840928fe5aa9c7d835f6306e59c1b
DELL <DELL@DESKTOP-M4KTLBQ>
2024-07-31 16:17:25 +0300

    feat: Initialize Question_Classes with basic Propeties

---
+++ OOP_Exam/Question_Classes/TrueOrFalseQuestion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Exam.Question_Classes
+{
+    internal class TrueOrFalseQuestion: Question
+    {
+        #region Constructor Chaining
+        public TrueOrFalseQuestion(string questionBody, double questionMark, List<Answer> questionAnswers, Answer correctAnswers)
+            :base(questionBody, questionMark, questionAnswers, correctAnswers)
+        {
+            QuestionType = "True | False";
+        }
+
+        #endregion
+
+    }
+}
