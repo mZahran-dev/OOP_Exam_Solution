d7e4bccaa777ce71d3093738f72b41fcedb3a4b8
DELL <DELL@DESKTOP-M4KTLBQ>
2024-07-31 16:08:47 +0300

    feat: initialize Exam and Subject Classes

---
+++ OOP_Exam/Exam_Classes/Exam.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Exam.Exam_Classes
+{
+    internal abstract class Exam
+    {
+        #region Properites
+        //private int examTime;
+        //private int numberOfQuestion;
+        //private List<Question>? questions;
+
+
+        #endregion
+    }
+}
