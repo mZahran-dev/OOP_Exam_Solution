316c34ffde39cf3fd277fa0e6dbea8dfb10768e3
DELL <DELL@DESKTOP-M4KTLBQ>
2024-07-31 17:48:58 +0300

    feat: Adding CreateMcqQuestion in QuestionFactory Class

--- OOP_Exam/QuestionFactory/QuestionFactory.cs
+++ OOP_Exam/QuestionFactory/QuestionFactory.cs
@@ -46,7 +46,40 @@ namespace OOP_Exam.QuestionFactory
 
         }
 
-        
+        public static McqQuestion CreateMcqQuestion()
+        {
+            Console.WriteLine("MCQ Question");
+            Console.Write("Enter Question Body: ");
+            string body = Console.ReadLine();
+
+            Console.Write("Enter Question Mark: ");
+            double mark = double.Parse(Console.ReadLine());
+
+            var answers = new List<Answer>();
+
+            Console.WriteLine("Choices Of Question: ");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"Please Enter Choice Number {i + 1}");
+                string answerText = Console.ReadLine();
+                answers.Add(new Answer(i + 1, answerText));
+            }
+            Console.WriteLine("Enter the right Answer Id: ");
+            int correctAnswerId = int.Parse(Console.ReadLine());
+
+            Answer correctAnswer = null;
+            foreach (var answer in answers)
+            {
+                if (answer.AnswerId == correctAnswerId)
+                {
+                    correctAnswer = answer;
+                    break;
+                }
+            }
+            return new McqQuestion(body, mark, answers, correctAnswer);
+
+        }
+
         #endregion
 
 
